74045346bc1c828e1cae39c6fb0812068ca9af54
s3ru <sebastian.d.ruf@proton.me>
2025-10-05 23:49:50 +0200

    Disable auto-format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,16 +2,16 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     css = { "prettier" },
-    html = { "prettier" }--,
-    --python = { "isort", "black" },
+    html = { "prettier" },
+    python = { "isort", "black" },
     --go = { "goimports", "gofmt" }
-  },
+  }, --,
 
-   format_on_save = {
-     -- These options will be passed to conform.format()
-     timeout_ms = 500,
-     lsp_fallback = true,
-  },
+  --format_on_save = {
+  -- These options will be passed to conform.format()
+  --  timeout_ms = 500,
+  --  lsp_fallback = true,
+  --},
 }
 
 return options
